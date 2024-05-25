430ba0170f22428c75e172df61e650e275168636
Tomoaki-Moriya <tomoaki.moriya1096@gmail.com>
2024-05-25 11:30:21 +0900

    first commit

---
+++ test/aws-cicd-receipe.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCicdReceipe from '../lib/aws-cicd-receipe-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cicd-receipe-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCicdReceipe.AwsCicdReceipeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
